01bdb08a941dabea4a3b4bcf9143bbb0ac097c46
Astika Luintel <astild2020@gmail.com>
2022-10-12 11:23:28 -0400

    commit tree a7f150c1caafb80f014c1704fd537961ac91c723

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    attr_reader :name, :breed
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
 
 end
+
+mydogs = Dog.new
+mydogs.name = "Dog"
+mydogs.breed = "breed"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-
+    attr_reader :name
+    attr_accessor :job
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
 end
+
+newname = Person.new()
+newname.name = "name"
+newname.job = "job"
